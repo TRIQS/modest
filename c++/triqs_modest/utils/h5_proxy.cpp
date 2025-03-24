378da09aab66dc50923bff7f92079289b3e43a01
Nils Wentzell <nwentzell@flatironinstitute.org>
2025-03-24 16:52:18 -0400

    Add minimal GPLv3 File Headers and generic COPYRIGHT notice
    - Move copyright notice into separate file COPYRIGHT
    - Rename LICENSE.txt file to LICENSE
    - Update default Copyright to Simons Foundation

--- c++/triqs_modest/utils/h5_proxy.cpp
+++ c++/triqs_modest/utils/h5_proxy.cpp
@@ -1,2 +1,7 @@
+// Copyright (c) 2025--present, The Simons Foundation
+// This file is part of TRIQS/modest and is licensed under the terms of GPLv3 or later.
+// SPDX-License-Identifier: GPL-3.0-or-later
+// See LICENSE in the root of this distribution for details.
+
 #include "./h5_proxy.hpp"
 namespace h5 {}
--- c++/triqs_modest/utils/to_vector.hpp
+++ c++/triqs_modest/utils/to_vector.hpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2025--present, The Simons Foundation
+// This file is part of TRIQS/modest and is licensed under the terms of GPLv3 or later.
+// SPDX-License-Identifier: GPL-3.0-or-later
+// See LICENSE in the root of this distribution for details.
+
 #pragma once
 #include <vector>
 
