51d56fb049810c924fe37643f37dab429d2ed8f2
jcoulter12 <jcoulter@flatironinstitute.org>
2025-07-31 11:29:08 -0400

    fix the use of Sigma_hartree in the test for obe_tb

--- c++/triqs_modest/obe_tb.cpp
+++ c++/triqs_modest/obe_tb.cpp
@@ -1,111 +1,146 @@
-#pragma once
-#include <triqs/lattice/tb_hamiltonian.hpp>
+#include <nda/layout/range.hpp>
+#include <nda/matrix_functions.hpp>
+#include <triqs/gfs.hpp>
+#include <triqs/utility/exceptions.hpp>
+#include <type_traits>
+#include "./local_space.hpp"
+#include <triqs/lattice/wannier_loader.hpp>
+#include "downfolding.hpp"
 #include <stdexcept>
-#include <triqs/mesh/imfreq.hpp>
-#include "loaders.hpp"
-#include "triqs/lattice/bz_integrators.hpp"
-#include "triqs/lattice/gloc.hpp"
-#include "triqs_modest/utils/gf_supp.hpp"
-#include <nda/nda.hpp>
-#include <triqs/gfs/functions/density.hpp>
+#include "./obe_tb.hpp"
+#include "triqs_modest/utils/defs.hpp"
 
-namespace nda::temp {
-  CLEF_MAKE_FNT_LAZY(trace);
-}
+// DANGEROUS : no way to check that wannier hamiltonian has atoms in the same order if user specifies their positions/
+// the atomic shell info. We are relying on them to get this right somehow, so there should be some print check
+//
+// TODO feels like discover_symmetries should have a default threshold; almost always sensible with that kind of thing
+// TODO what do about sigma_hartree... forward it to gloc in triqs as something optional?
 
 namespace triqs::modest {
 
-  struct one_body_elements {
-    local_space C_space;
-    C2PY_IGNORE std::vector<tb_hamiltonian> H; // positibility to have two spin channels for spin pol
-    //downfolding_projector P;
-    //C2PY_IGNORE std::optional<ibz_symmetry_ops> ibz_symm_ops = {}; //< IBZ symmetrizer after a k-sum
-  };
-
   /** Construct a obe_tb from Wannier90 in the case of a single spin index 
-   * @param wannier_file_path string to Wannier90 files, including the prefix 
+   * @param wannier_file_path string to Wannier90 files, including the prefix // FIXME example 
    * @param spin_kind enum telling us the spintype 
    * @param shells list of atomic shells input by the user 
    */
   one_body_elements one_body_elements_from_wannier90(std::string const &wannier_file_path, spin_kind_e const &spin_kind,
-                                                     std::vector<atomic_shell_t> atomic_shells);
+                                                     std::vector<atomic_shell_t> atomic_shells) {
+    if (spin_kind == spin_kind_e::Polarized) {
+      throw std::runtime_error("If performing a spin-polarized calculation, you need to supply two Wannier file paths for up and down channels.\n");
+    }
+    // call the wannier90 loader and set up tb_hamiltonian
+    // FIXME check if tb or hr exists -- write a lambda that checks which exists and returns just one of the two
+    auto [R, HR, _] = read_wannier90_tb_data(wannier_file_path);
+    std::vector<tb_hamiltonian> tb_H;
+    tb_H.emplace_back(R, HR);
+    return make_obe_from_tb(std::move(tb_H), spin_kind, std::move(atomic_shells));
+  };
 
   one_body_elements one_body_elements_from_wannier90(std::string const &wannier_file_path_up, std::string const &wannier_file_path_dn,
-                                                     spin_kind_e const &spin_kind, std::vector<atomic_shell_t> atomic_shells);
+                                                     spin_kind_e const &spin_kind, std::vector<atomic_shell_t> atomic_shells) {
+    if (spin_kind != spin_kind_e::Polarized) {
+      throw std::runtime_error("For a non-spin polarized calculation, you should specify only one Wannier Hamiltonian.\n");
+    }
 
-  // TODO docstring
-  nda::array<nda::matrix<dcomplex>, 2> Hloc(std::vector<tb_hamiltonian> const &H_sigma, std::vector<atomic_shell_t> const &atomic_shells);
-  nda::array<nda::matrix<dcomplex>, 2> Hloc(one_body_elements const &obe);
-  nda::array<nda::matrix<dcomplex>, 2> impurity_levels(one_body_elements_on_grid const &obe);
+    // call the wannier90 loader and set up tb_hamiltonian list
+    std::vector<tb_hamiltonian> tb_H;
+    for (auto file : {wannier_file_path_up, wannier_file_path_dn}) {
+      auto [R, HR, _] = read_wannier90_tb_data(file);
+      tb_H.emplace_back(R, HR);
+    }
+    if (tb_H[0].n_orbitals() != tb_H[1].n_orbitals()) {
+      throw std::runtime_error(
+         "Cannot construct a one_body_elements "
+         "using up and down H_k that have a different number of orbtials.");
+    }
+    return make_obe_from_tb(std::move(tb_H), spin_kind, std::move(atomic_shells));
+  }
 
-  C2PY_IGNORE one_body_elements make_obe_from_tb(std::vector<tb_hamiltonian> const tb_H_sigma, spin_kind_e const &spin_kind,
-                                                 std::vector<atomic_shell_t> atomic_shells);
+  // -----------------------------------------------------------------------
 
-  /** @brief Compute local Green's function from a one_body_elements_tb object.
-    *    
-    * @param one_body_elements_tb A one_body_elements object containing the tb_hamiltonian
-    * @param mu Chemical potential 
-    * @param Sigma_embed Self energy in the embedded basis? TODO I don't think this was relevant 
-    * @param Sigma_DC ? TODO JC would like this to be optional 
-    * @param bz_int_options Option of the BZ integration
-    * @param d_H ? for magnetic fields etc? 
-    * @return gloc[alpha][sigma] = gf on the mesh, as a vector of length alpha, storing block gfs dim sigma 
-    */
-  template <typename Mesh>
-  block2_gf<Mesh, matrix_valued> gloc(one_body_elements const &obe, double mu, block2_gf<Mesh, matrix_valued> const &Sigma_dynamic,
-                                      nda::array<nda::matrix<dcomplex>, 2> const &Sigma_hartree, triqs::lattice::bz_int_options const &opt) {
+  //TODO docstring
+  /** Helper function to calculate Hloc given a vector of H with length sigma (contained in Wannier OBE)
+   */
+  nda::array<nda::matrix<dcomplex>, 2> Hloc(std::vector<tb_hamiltonian> const &H_sigma, std::vector<atomic_shell_t> const &atomic_shells) {
 
-    // Need a sigma Hartree
-    auto &mesh       = Sigma_dynamic(0, 0).mesh();
-    auto n_sigma     = Sigma_dynamic.size2();
-    auto gloc_result = make_block2_gf(mesh, obe.C_space.Gc_block_shape());
+    // return Hloc with shape [atomic_shells, nsigma]
+    nda::array<nda::matrix<dcomplex>, 2> Hloc_result(atomic_shells.size(), H_sigma.size());
 
-    if (n_sigma != obe.H.size()) { throw std::runtime_error("Mismatch between the spin channels in Sigma and spin channels in Hamiltonian."); }
+    for (auto [isigma, H] : enumerate(H_sigma)) {
 
-    // Embedding decomposition from structure of Sigma -- provides a list of block names
-    auto embedding_decomp = get_struct(Sigma_dynamic).dims(r_all, 0) | tl::to<std::vector>();
+      // check that HR and atomic shells list have the same total size
+      long n_orb = 0;
+      for (auto shell : atomic_shells) { n_orb += shell.dim; }
+      if (H.n_orbitals() != n_orb) {
+        throw std::runtime_error("Wannier Hamiltonian does not have the same number of orbitals as the provided atomic shells: HR " + std::to_string(H.n_orbitals()) + 
+                            " , atomic_shells total " + std::to_string(n_orb));
+      }
+
+      // find the home cell of the TB file to get H0
+      auto iR0 = H.get_R_idx({0, 0, 0});
+
+      // Hloc needs to have dimensions [nshells, nspin].
+      // REFACTOR -- need opposite of enumerate_sub_slice --
+      // NOTE: we cannot use enumerated_subslice here because we do not have a C_space!
+      // this function computes Hloc so we can determine the C_space in the first place.
+      long start_orb = 0;
+      for (auto &&[ishell, shell] : enumerate(atomic_shells)) {
+        auto Hloc0_ab = nda::zeros<dcomplex>(shell.dim, shell.dim);
+        // need to create a matrix of dim x dim for each
+        for (auto iorb : nda::range(shell.dim)) {
+          for (auto jorb : nda::range(shell.dim)) { Hloc0_ab(iorb, jorb) = H.hoppings()[iR0](start_orb + iorb, start_orb + jorb); }
+          start_orb += shell.dim;
+        }
+        Hloc_result(ishell, isigma) = Hloc0_ab;
+      }
+    }
+    return Hloc_result;
+  }
+
+  /** Creates an Hloc function from an OBE based on a tight binding Hamiltonian 
+   */
+  nda::array<nda::matrix<dcomplex>, 2> Hloc(one_body_elements const &obe) {
+
+    // TODO is this working as we think it will... FIXME need num atoms here
+    auto n_atoms = obe.C_space.n_atoms();
+    auto n_sigma = obe.C_space.n_sigma();
+    nda::array<nda::matrix<dcomplex>, 2> Hloc_result(n_atoms, n_sigma);
 
     for (auto sigma : range(n_sigma)) {
 
-      // spin index
-      auto Sigma_big_space = gfs::gf(mesh, {obe.H[sigma].n_orbitals(), obe.H[sigma].n_orbitals()}); //
-      for (auto &&[block, R] : enumerated_sub_slices(embedding_decomp)) { Sigma_big_space.data()(r_all, R, R) = Sigma_dynamic(block, sigma).data(); }
+      // find the home cell of the TB file to get H0
+      auto iR0 = obe.H[sigma].get_R_idx({0, 0, 0});
 
-      // Call the TRIQS version of this function
-      gloc_result(0, sigma) = gloc(obe.H[sigma], mu, Sigma_big_space, opt);
+      for (auto const &[atom, R_atom] : enumerated_sub_slices(obe.C_space.atomic_decomposition())) {
+        //auto shell               = obe.C_space.atomic_shells()[atom];
+        Hloc_result(atom, sigma) = obe.H[sigma].hoppings()[iR0](R_atom, R_atom);
+      }
     }
-    return gloc_result;
+    return Hloc_result;
   }
 
-  template <typename Mesh>
-  double density(one_body_elements const &obe, double mu, block2_gf<Mesh, matrix_valued> const &Sigma_dynamic,
-                 nda::array<nda::matrix<dcomplex>, 2> const &Sigma_hartree, triqs::lattice::bz_int_options const &opt) {
+  // -----------------------------------------------------------------------
 
-    auto const &mesh = Sigma_dynamic(0, 0).mesh();
-    auto beta        = mesh.beta();
+  one_body_elements make_obe_from_tb(std::vector<tb_hamiltonian> H_sigma, spin_kind_e const &spin_kind, std::vector<atomic_shell_t> atomic_shells) {
 
-    //auto n_blocks = Sigma_dynamic.size1();
-    auto n_sigma = Sigma_dynamic.size2();
+    // calculate Hloc using helper function -- Hloc here is dim [nshells, nsigma]
+    nda::array<nda::matrix<dcomplex>, 2> hloc = Hloc(H_sigma, atomic_shells);
 
-    double n  = 0;
-    auto Gloc = gloc(obe, mu, Sigma_dynamic, Sigma_hartree, opt); // returns block2gf (1, nsigma, {norb, norb})
-    // return type of Gloc is a B2GF with dimensions (1, nspin, {norb, norb})
-    for (auto sigma : range(n_sigma)) { // spin index
-      n += real(nda::trace(density(Gloc(0, sigma))));
-    }
-    return n;
+    // construct block structure using Hloc
+    double block_threshold  = 1e-6;
+    bool diagonalize_hloc   = false; // CHECK this would mean looking at band energies at k = 0?
+    auto [decomposition, U] = discover_symmetries(hloc, atomic_shells, block_threshold, diagonalize_hloc);
+
+    // call constructor for local space, for now ignoring rotation_from_spherical_to_dft_basis
+    auto LS = local_space(spin_kind, atomic_shells, decomposition, U, {});
+
+    // construct and return obe_tb
+    return one_body_elements{.C_space = std::move(LS), .H = std::move(H_sigma)};
   }
 
-  // --------  instantiations --------------
-
-  /** @cond DOXYGEN_SKIP_THIS */
-  block2_gf<mesh::imfreq, matrix_valued> gloc(one_body_elements const &obe, double mu, block2_gf<mesh::imfreq, matrix_valued> const &Sigma_dynamic,
-                                              nda::array<nda::matrix<dcomplex>, 2> const &Sigma_hartree, double tolerance,
-                                              triqs::lattice::bz_int_options const &opt);
-
-  //double density(one_body_elements const &obe, double mu, block2_gf<mesh::imfreq, matrix_valued> const &Sigma_dynamic,
-  //               nda::array<nda::matrix<dcomplex>, 2> const &Sigma_hartree);
-
-  /** @endcond */
+  /** Compute the local impurity levels from the single-particle dispersion. 
+  * This simply computes Hloc and returns it, but exists to match the syntax used in the fixed_grid case. 
+  */
+  nda::array<nda::matrix<dcomplex>, 2> impurity_levels(one_body_elements const &obe) { return Hloc(obe); }
 
 } // namespace triqs::modest
--- test/c++/obe_tb_test.cpp
+++ test/c++/obe_tb_test.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
+#include <nda/basic_functions.hpp>
 #include <nda/gtest_tools.hpp>
+#include <nda/matrix_functions.hpp>
+#include <triqs/arrays.hpp>
 #include <triqs/mesh/imfreq.hpp>
 #include "triqs_modest/gloc_fixed_grid.hpp"
 #include "triqs_modest/obe_tb.hpp"
@@ -21,22 +24,23 @@ TEST(obe_tb, lco_wannier90) { // NOLINT
   atomic_shells.emplace_back(dim, l, type, atom);
 
   // load in a obe from a Wannier tb model
-  one_body_elements obe_tb = one_body_elements_from_wannier90("../../../test/c++/ref_data/lco", spin_kind_e::NonPolarized, atomic_shells);
+  one_body_elements obe_tb = one_body_elements_from_wannier90("./ref_data/lco", spin_kind_e::NonPolarized, atomic_shells);
   double beta              = 10.;
   auto iw_mesh             = mesh::imfreq{beta, triqs::mesh::Fermion, 51};
 
   // make a zero self energy
   auto Sigma        = gfs::gf<mesh::imfreq>{iw_mesh, {dim, dim}};
   auto Sigma_block2 = make_block2_gf(1, 1, Sigma); // block, nsigma, fill with empty sigma
-  nda::array<nda::matrix<dcomplex>, 2> Sigma_hartree;
+  nda::array<nda::matrix<dcomplex>, 2> Sigma_hartree(1, 1);
+  Sigma_hartree(0,0) = nda::matrix<dcomplex>::zeros(1,1); // 1 orbital 
 
   // read in some reference data
-  auto [dft_density, obe_dft] = one_body_elements_from_dft_converter("../../../test/c++/ref_data/lco_wannier.h5");
+  auto [dft_density, obe_dft] = one_body_elements_from_dft_converter("./ref_data/lco_wannier.ref.h5");
   double mu                   = find_chemical_potential(dft_density, obe_dft, beta);
   auto gloc_dft               = gloc(iw_mesh, obe_dft, mu);
 
   // run gloc, forcing gloc to use PTR on the same grid as the DFT calculation was done
-  triqs::bz_int::bz_int_options opt = {.k_grid_dims = {7, 7, 7}, .n_k_max = 8, .run_adaptive = false};
+  triqs::lattice::bz_int_options opt = {.k_grid_dims = {7, 7, 7}, .n_k_max = 8, .run_adaptive = false};
   auto gloc_tb                      = gloc(obe_tb, mu, Sigma_block2, Sigma_hartree, opt);
 
   // Check equivalence
@@ -44,5 +48,5 @@ TEST(obe_tb, lco_wannier90) { // NOLINT
 
   // density calculation
   //std::cout << density_obe(obe_tb, mu, Sigma_block2, Sigma_hartree) << std::endl;
-  std::cout << density(gloc_tb(0, 0)) << std::endl;
+  //std::cout << density(gloc_tb(0, 0)) << std::endl;
 }
