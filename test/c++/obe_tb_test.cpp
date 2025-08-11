8e117dbbb9b3f3f999f7d050ce0ba42a806ce1d5
jcoulter12 <jcoulter@flatironinstitute.org>
2025-08-11 11:33:19 -0400

    wip of sr2moo4 test (non orthogonal cell) for LFS

--- test/c++/obe_tb_test.cpp
+++ test/c++/obe_tb_test.cpp
@@ -14,7 +14,7 @@ using namespace triqs::modest;
 using namespace triqs::gfs;
 using namespace triqs;
 
-// REFACTOR This is partially duplicate with density_test.cpp 
+// REFACTOR This is partially duplicate with density_test.cpp
 double random_constant() {
   std::mt19937 gen(std::random_device{}());
   std::uniform_real_distribution<double> dist(0, 1);
@@ -25,8 +25,8 @@ double random_constant() {
 auto eps   = 1.e-12;
 auto w_max = 10.0;
 
-auto make_random_self(int M, double beta) {
-  auto iw_mesh  = mesh::imfreq{beta, triqs::mesh::Fermion, 51}; 
+auto make_random_self(long M, double beta) {
+  auto iw_mesh     = mesh::imfreq{beta, triqs::mesh::Fermion, 51};
   auto g           = gfs::gf<mesh::imfreq>{iw_mesh, {M, M}};
   auto const &mesh = g.mesh();
   auto A           = random_constant();
@@ -104,16 +104,16 @@ TEST(obe_tb, svo_t2g_wannier90_multiorbtial) { // NOLINT
   one_body_elements_tb obe_tb = one_body_elements_from_wannier90("./ref_data_lfs/svo_t2g", spin_kind_e::NonPolarized, atomic_shells);
   double beta                 = 10.;
   auto iw_mesh                = mesh::imfreq{beta, triqs::mesh::Fermion, 51};
-  long n_sigma      = 2;
-  long n_atoms = 1; 
+  long n_sigma                = 2;
+  long n_atoms                = 1;
 
-  // TODO replace this with function calls that don't take self energy 
+  // TODO replace this with function calls that don't take self energy
   // make a zero self energy
   auto Sigma        = gfs::gf<mesh::imfreq>{iw_mesh, {dim, dim}};
   auto Sigma_block2 = make_block2_gf(n_atoms, n_sigma, Sigma); // block, nsigma, fill with empty sigma
   nda::array<nda::matrix<dcomplex>, 2> Sigma_static(n_atoms, n_sigma);
   // spin up and down both zero for now, 3 orbitals in the single block
-  for (auto sigma : nda::range(n_sigma)) { Sigma_static(0, sigma) = nda::diag(nda::vector<dcomplex>{0.,0.,0.}); }
+  for (auto sigma : nda::range(n_sigma)) { Sigma_static(0, sigma) = nda::diag(nda::vector<dcomplex>{0., 0., 0.}); }
 
   // read in some reference data
   auto [dft_density, obe_dft] = one_body_elements_from_dft_converter("./ref_data_lfs/svo_t2g_w90_fixedgrid.ref.h5");
@@ -121,8 +121,8 @@ TEST(obe_tb, svo_t2g_wannier90_multiorbtial) { // NOLINT
   auto gloc_dft               = gloc(iw_mesh, obe_dft, mu_dft);
 
   // run gloc, forcing gloc to use PTR on the same grid as the DFT calculation was done
-  triqs::lattice::bz_int_options opt = {.k_grid = {5, 5, 5}, .k_grid_max = {5,5,5}, .run_adaptive = false};
-  auto gloc_tb = gloc(iw_mesh, obe_tb, mu_dft, opt);
+  triqs::lattice::bz_int_options opt = {.k_grid = {5, 5, 5}, .k_grid_max = {5, 5, 5}, .run_adaptive = false};
+  auto gloc_tb                       = gloc(iw_mesh, obe_tb, mu_dft, opt);
 
   // FIXME : something about this seems potentially wrong
   // Check equivalence -- note, this is a very coarse DFT grid, it seems Wannierization has a tiny variation that
@@ -149,17 +149,17 @@ TEST(obe_tb, svo_t2g_wannier90_multiorbtial) { // NOLINT
   for (auto [is, shell] : enumerate(atomic_shells)) {
     for (auto sigma : nda::range(n_sigma)) { EXPECT_ARRAY_NEAR(impurity_levels(obe_dft)(is, sigma), impurity_levels(obe_tb)(is, sigma), 1e-8); }
   }
-  
+
   // make a self energy, call self energy interfaces --------------------
   auto Sigma_single = make_random_self(obe_tb.H[0].n_orbitals(), beta);
-  auto Sigma_dyn = make_block2_gf(n_atoms, n_sigma, Sigma_single); 
+  auto Sigma_dyn    = make_block2_gf(n_atoms, n_sigma, Sigma_single);
   // spin up/down, 3 orbitals in the single block
-  for (auto sigma : nda::range(n_sigma)) { Sigma_static(0, sigma) = nda::diag(nda::vector<dcomplex>{0.6,0.6,0.8}); }
+  for (auto sigma : nda::range(n_sigma)) { Sigma_static(0, sigma) = nda::diag(nda::vector<dcomplex>{0.6, 0.6, 0.8}); }
 
-  // try with a self energy 
-  // TODO why does this not return exactly the same type as the earlier function call? 
-  auto gloc_tb_SE                       = gloc(obe_tb, mu_dft, Sigma_dyn, Sigma_static, opt);
-  auto gloc_dft_SE                      = gloc(obe_dft, mu_dft, Sigma_dyn, Sigma_static);
+  // try with a self energy
+  // TODO why does this not return exactly the same type as the earlier function call?
+  auto gloc_tb_SE  = gloc(obe_tb, mu_dft, Sigma_dyn, Sigma_static, opt);
+  auto gloc_dft_SE = gloc(obe_dft, mu_dft, Sigma_dyn, Sigma_static);
 
   for (auto iw : iw_mesh) { //
     if (std::abs(dcomplex(iw.value())) < 1.7) {
@@ -169,72 +169,64 @@ TEST(obe_tb, svo_t2g_wannier90_multiorbtial) { // NOLINT
     }
   }
 }
-#endif
-
 /*
-TEST(obe_tb, svo_t2g_eg_wannier90) { // NOLINT
+TEST(obe_tb, sr2moo4_wannier90) { // NOLINT
 
   // set up atomic shells
   std::vector<atomic_shell_t> atomic_shells;
-
-  // a shell with 3 d orbitals on the V atom
-  long dim  = 2;
+  long dim  = 3;
   long l    = 2;
   long type = 0;
   long atom = 0;
   atomic_shells.emplace_back(dim, l, type, atom);
-  // set up SVO shells, trying to keep the same order as the wannier hamiltonian does
-  // a shell with 3 d orbitals on the V atom
-  dim  = 3;
-  l    = 2;
-  type = 0;
-  atom = 0;
-  atomic_shells.emplace_back(dim, l, type, atom);
 
   // load in a obe from a Wannier tb model
-  one_body_elements_tb obe_tb =
-     one_body_elements_from_wannier90("../../../test/c++/ref_data_lfs/svo_t2g_eg", spin_kind_e::NonPolarized, atomic_shells);
-  double beta  = 10.;
-  auto iw_mesh = mesh::imfreq{beta, triqs::mesh::Fermion, 51};
+  one_body_elements_tb obe_tb = one_body_elements_from_wannier90("./ref_data_lfs/sr2moo4", spin_kind_e::NonPolarized, atomic_shells);
+  double beta                 = 10.;
+  auto iw_mesh                = mesh::imfreq{beta, triqs::mesh::Fermion, 51};
 
   // make a zero self energy
-  long n_blocks     = 1;
-  long n_sigma      = 2;
-  auto Sigma        = gfs::gf<mesh::imfreq>{iw_mesh, {dim, dim}};
-  auto Sigma_block2 = make_block2_gf(n_blocks, n_sigma, Sigma); // block, nsigma, fill with empty sigma
-  nda::array<nda::matrix<dcomplex>, 2> Sigma_hartree(n_blocks, n_sigma);
-  // spin up and down both zero for now, 3 orbitals in the single block
-  for (auto shell : atomic_shells) {
-    for (auto sigma : nda::range(n_sigma)) { Sigma_hartree(0, sigma) = nda::matrix<dcomplex>::zeros(shell.dim, shell.dim); }
-  }
+  long n_sigma = 2;
+  long n_atoms = 1;
+
+  // make a self energy, call self energy interfaces --------------------
+  auto Sigma_single = make_random_self(obe_tb.H[0].n_orbitals(), beta);
+  auto Sigma_dyn    = make_block2_gf(n_atoms, n_sigma, Sigma_single);
+  nda::array<nda::matrix<dcomplex>, 2> Sigma_static(1, n_sigma);
+  // spin up/down, 3 orbitals in the single block, nonsense inf omega SE
+  for (auto sigma : nda::range(n_sigma)) { Sigma_static(0, sigma) = nda::diag(nda::vector<dcomplex>{0.6, 0.5, 0.9}); }
 
   // read in some reference data
-  auto [dft_density, obe_dft] = one_body_elements_from_dft_converter("../../../test/c++/ref_data_lfs/svo_t2g_eg.h5");
-  double mu_dft               = find_chemical_potential(dft_density, obe_dft, beta);
-  auto gloc_dft               = gloc(iw_mesh, obe_dft, mu_dft);
-
+  // REFACTOR: put all the args so I can set verbotisty to false on find mu?
+  auto [dft_density, obe_dft] = one_body_elements_from_dft_converter("./ref_data_lfs/sr2moo4_noshift.h5");
+  double mu_dft               = 0;
+  {
+    //scoped_timer timer;
+    //mu_dft = find_chemical_potential(dft_density, obe_dft, Sigma_dyn, Sigma_static);
+    //auto gloc_dft               = gloc(obe_dft, mu_dft, Sigma_dyn, Sigma_static);
+  }
   // run gloc, forcing gloc to use PTR on the same grid as the DFT calculation was done
-  triqs::lattice::bz_int_options opt = {.k_grid_dims = {5, 5, 5}, .n_k_max = 6, .run_adaptive = false};
-  auto gloc_tb                       = gloc(obe_tb, mu_dft, Sigma_block2, Sigma_hartree, opt);
-
-  // Check equivalence
-  for (auto iw : iw_mesh) { EXPECT_COMPLEX_NEAR(gloc_tb(0, 0)[iw](0, 0), gloc_dft(0, 0)[iw](0, 0), 1e-5); }
-
-  // density calculation for one spin channel
-  double n = density(obe_tb, mu_dft, Sigma_block2, Sigma_hartree, opt);
-  // calculation does not give exactly 1 for density because of inaccurate int paramters
-  EXPECT_NEAR(n, 1.0063915056934261, 1e-4);
+  triqs::lattice::bz_int_options opt = {.run_ptr = false}; //{.k_grid = {40, 40, 40}, .k_grid_max = {40, 40, 40}, .run_adaptive = false};
+  //auto gloc_tb                       = gloc(obe_tb, mu_dft, Sigma_dyn, Sigma_static, opt);
 
   // check mu finding
-  double mu_calc = find_chemical_potential(n, obe_tb, Sigma_block2, Sigma_hartree, opt);
-  EXPECT_NEAR(mu_calc, mu_dft, 1e-4);
+  double mu_tb = 0;
+  {
+    scoped_timer timer;
+    mu_tb = find_chemical_potential(2.0, obe_tb, Sigma_dyn, Sigma_static, opt);
+  }
+  //EXPECT_NEAR(mu_tb, mu_dft, 1e-4);
 
-  //for (auto sh : obe_dft.C_space.atomic_shells()) { std::cout << sh.dim << std::endl; }
+  // density calculation
+  //double n = density(obe_tb, mu_tb, Sigma_dyn, Sigma_static, opt);
+  //EXPECT_NEAR(n, 2.0, 1e-4);
 
-  // check spin up and down channels
-  //for (auto [is, shell] : enumerate(atomic_shells)) {
-  //for (auto sigma : nda::range(n_sigma)) { EXPECT_ARRAY_NEAR(impurity_levels(obe_dft)(is, sigma), impurity_levels(obe_tb)(is, sigma), 1e-8); }
-  //std::cout << impurity_levels(obe_dft) << std::endl;
-  //std::cout << impurity_levels(obe_tb) << std::endl;
-  //}
+  // check gloc is the same
+  //for (auto iw : iw_mesh) { EXPECT_COMPLEX_NEAR(gloc_tb(0, 0)[iw](0, 0), gloc_dft(0, 0)[iw](0, 0), 1e-5); }
+
+  // check impurity levels are the same
+  for (auto sigma : nda::range(n_sigma)) { EXPECT_ARRAY_NEAR(impurity_levels(obe_dft)(0, sigma), impurity_levels(obe_tb)(0, sigma), 1e-8); }
 } */
+#endif
+
+MPI_TEST_MAIN
