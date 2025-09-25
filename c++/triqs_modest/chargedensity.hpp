1c80065f17561bad80e61ab860201c9f42fed2e5
Harrison LaBollita <harrisonlabollita@gmail.com>
2025-09-25 09:56:33 -0400

    draft charge density correction for csc; to be tested

---
+++ c++/triqs_modest/chargedensity.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "utils/defs.hpp"
+#include "utils/gf_supp.hpp"
+#include <triqs/mesh.hpp>
+#include "downfolding.hpp"
+#include "gloc_fixed_grid.hpp"
+
+namespace triqs::modest {
+
+/**
+ * @brief Compute the charge density correction from DMFT
+ *
+ * Compute the charge density correction in the band basis \f$ N_{\nu\nu'}(\mathbf{k}) \f$ from the lattice Green's function.
+ * 
+ * @tparam Mesh The mesh type
+ * @param obe The one-body elements on the grid
+ * @param mu The chemical potential
+ * @param Sigma_dynamic The dynamic part of the self-energy
+ * @param Sigma_static The static part of the self-energy
+ * @return The charge density correction in the band basis \f$ N_{\nu\nu'}(\mathbf{k}) \f$
+ */
+  template <typename Mesh>
+  nda::array<dcomplex, 4> charge_density_correction(one_body_elements_on_grid const &obe, double mu,
+                                                    block2_gf<Mesh, matrix_valued> const &Sigma_dynamic,
+                                                    nda::array<nda::matrix<dcomplex>, 2> const &Sigma_static) {
+
+    auto n_sigma = obe.C_space.n_sigma(); // number of spin channesl
+    auto n_k     = obe.H.n_k();           // number of k-points
+
+    // maximum number of bands
+    auto n_nu_k = range(n_k) | stdv::transform([&obe](auto k_idx) { return obe.H.N_nu(0, k_idx); }) | tl::to<std::vector>();
+    auto n_nu   = *std::max_element(begin(n_nu_k), end(n_nu_k));
+
+    // setup data structure for return
+    auto N_nu_nup_k = nda::zeros<dcomplex>(n_k, n_sigma, n_nu, n_nu);
+
+    // calculator for lattice Green's function at given k and σ
+    auto glatt_at_k = detail::lattice_gf_at_k(obe, mu, Sigma_dynamic, Sigma_static);
+
+    // loop over k and σ 
+    // TODO: parallelize over k and gather
+    for (auto k_idx : range(n_k)) {
+      for (auto const &sigma : range(n_sigma)) { N_nu_nup_k(k_idx, sigma, r_all, r_all) = density(glatt_at_k(k_idx, sigma)); }
+    }
+
+    return N_nu_nup_k;
+  }
+
+  template nda::array<dcomplex, 4> charge_density_correction(one_body_elements_on_grid const &obe, double mu,
+                                                             block2_gf<imfreq, matrix_valued> const &Sigma_dynamic,
+                                                             nda::array<nda::matrix<dcomplex>, 2> const &Sigma_static);
+
+} // namespace triqs::modest
---
+++ test/c++/charge_update.cpp
@@ -0,0 +1,25 @@
+#include "./common.hpp"
+#include "triqs_modest/chargedensity.hpp"
+
+
+// TODO: Test against dft_tools.sum_kdft.calc_density_correction
+auto run_charge_update_case(std::string filename) {
+
+  auto [target_density, obe] = one_body_elements_from_dft_converter(filename);
+  auto E                     = make_embedding(obe.C_space);
+
+  auto const &mesh = mesh::imfreq{40.0, statistic_enum::Fermion, 1025};
+
+  auto Sigma_imps        = E.make_zero_imp_self_energies(mesh);
+  auto Sigma_imp_dynamic = Sigma_imps | stdv::transform([](auto &x) { return x.first; }) | tl::to<std::vector>();
+  auto Sigma_imp_static  = Sigma_imps | stdv::transform([](auto &x) { return x.second; }) | tl::to<std::vector>();
+
+  auto [Sigma_embed, Sigma_H_embed] = E.embed(Sigma_imp_dynamic, Sigma_imp_static);
+  auto mu                           = find_chemical_potential(target_density, obe, Sigma_embed, Sigma_H_embed);
+  auto N_nu_nup_k                   = charge_density_correction(obe, mu, Sigma_embed, Sigma_H_embed);
+  return 0;
+}
+
+TEST(charge_update, charge_wien2k_svo3) { // NOLINT
+  run_charge_update_case("ref_data/SrVO3-cubic-t2g.ref.h5");
+}
