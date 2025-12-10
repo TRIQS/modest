0f62089ecb2c6a8ec5b90a6b716c681473e1915f
Harrison LaBollita <harrisonlabollita@gmail.com>
2025-12-10 13:23:23 -0500

    fix dft driver with mpi changes to load funcs

--- python/triqs_modest/dft_driver/driver.py
+++ python/triqs_modest/dft_driver/driver.py
@@ -3,11 +3,12 @@ import triqs.utility.mpi as mpi
 from triqs_modest import one_body_elements_from_dft_converter
 
 class DftDriver(object):
-    def __init__(self, DftSpecficDriver):
+    def __init__(self, DftSpecificDriver):
         required_methods = ['run_initial_stage', 'run_update_stage']
-        for m in require_methods:
-            if not hasattr(DftSpecficDriver, m) or not callable(getattr(DftSpecficDriver, m)):
-                raise TypeError(f"{DftSpecficDriver.__name__} must define a callable method {m}")
+        for m in required_methods:
+            if not hasattr(DftSpecificDriver, m) or not callable(getattr(DftSpecificDriver, m)):
+                raise TypeError(f"{DftSpecificDriver.__name__} must define a callable method {m}")
+
         self.driver = DftSpecificDriver
 
     def one_body_elements_from_dft(self, threshold=1e-5, diagonalize_hloc=False, **kwargs) :
@@ -15,25 +16,17 @@ class DftDriver(object):
         # DFT code specific initial stage
         status = self.driver.run_initial_stage(**kwargs)
         
-        target_density, obe = None, None
-        if mpi.is_master_node():
-            target_density, obe = one_body_elements_from_dft_converter(f"{self.driver.seedname}.h5", 
-                                  threshold=threshold, 
-                                  diagonalize_hloc=diagonalize_hloc
-                                  )
-        target_density, obe = mpi.bcast(target_density), mpi.bcast(obe)
-        return target_density, obe
+        return one_body_elements_from_dft_converter(f"{self.driver.seedname}.h5", 
+                                threshold=threshold, 
+                                diagonalize_hloc=diagonalize_hloc
+                                )
 
     def update_one_body_elements_with_charge_correction(self, N_k, Eint_m_dc, threshold=1e-5, diagonalize_hloc=False, **kwargs):
         
         # DFT code specific update stage
         status = self.driver.run_update_stage(N_k, Eint_m_dc, **kwargs)
 
-        target_density, obe = None, None
-        if mpi.is_master_node():
-            target_density, obe = one_body_elements_from_dft_converter(f"{self.driver.seedname}.h5", 
+        return one_body_elements_from_dft_converter(f"{self.driver.seedname}.h5", 
                                   threshold=threshold, 
                                   diagonalize_hloc=diagonalize_hloc
                                   )
-        target_density, obe = mpi.bcast(target_density), mpi.bcast(obe)
-        return target_density, obe
